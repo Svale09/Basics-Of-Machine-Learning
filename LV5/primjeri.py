8b719072b8c9d93909af6d5778dbddee987f5a77
Svale09 <svalinaivan09+git@gmail.com>
2023-03-29 22:06:49 +0200

    Example 1

---
+++ LV5/primjeri.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+yTrue = np.array([1,1,1,0,1,0,1,0,1])
+yPred = np.array([0,1,1,1,1,0,1,0,0])
+
+cm = confusion_matrix(yTrue, yPred)
+print("Matrica zabune: ", cm)
+disp = ConfusionMatrixDisplay(confusion_matrix(yTrue, yPred))
+disp.plot()
+plt.show()
