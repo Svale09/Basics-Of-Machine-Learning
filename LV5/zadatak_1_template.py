1961abf49a2e2e90b5198bb6c91ccb6fde3844ae
Svale09 <svalinaivan09+git@gmail.com>
2023-03-30 09:58:35 +0200

    Finished d) subtask

--- LV5/zadatak_1_template.py
+++ LV5/zadatak_1_template.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, classification_report
 
 
 X, y = make_classification(n_samples=200, n_features=2, n_redundant=0, n_informative=2,
@@ -14,4 +17,32 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 plt.scatter(X_train[:,0], X_train[:,1], s=3, c=y_train,cmap = "bwr")
 plt.scatter(X_test[:,0], X_test[:,1], c=y_test, marker="x", cmap="bwr")
+
+LogRegression_model = LogisticRegression()
+LogRegression_model.fit(X_train,y_train)
+
+y_test_p = LogRegression_model.predict(X_test)
+
+print(LogRegression_model.intercept_)
+print(LogRegression_model.coef_)
+
+b = LogRegression_model.intercept_
+w1, w2 = LogRegression_model.coef_.T
+
+c = -b/w2
+m = -w1/w2
+
+xmin, xmax = -4, 4
+ymin, ymax = -4, 4
+xd = np.array([xmin, xmax])
+yd = m*xd+c
+
+plt.plot(xd, yd, "k", lw = 1, ls = "--")
 plt.show()
+
+cm = confusion_matrix(y_test, y_test_p)
+disp = ConfusionMatrixDisplay(confusion_matrix(y_test, y_test_p))
+disp.plot()
+plt.show()
+
+print(classification_report(y_test, y_test_p))
